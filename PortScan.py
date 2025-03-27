39bc957604e96b22b268765ec5fe57774d050224
ManojGnanapalam <manojgsret@gmail.com>
2025-03-27 20:39:51 +0530

    PortScan Updated

--- PortScan.py
+++ PortScan.py
@@ -1,21 +1,37 @@
 import socket
 import sys
+import errno
+from datetime import datetime
 
-def checkPortsSocket(ip,portlist):
-    try:
-        for port in portlist:
-            sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            sock.settimeout(5)
-            result = sock.connect_ex((ip,port))
-            if result == 0:
-                print("Port{}:\t Open".format(port))
-            else:
-                print("Port{}:\t Closed".format(port))
-            sock.close()
-    except socket.error as error:
-        print(str(error))
-        print("Connection error")
-        sys.exit()
+target = input("Enter the target to scan :")
+targetIP = socket.gethostbyname(target)
 
+print("Enter the range of ports to scan")
+startPort = input("Enter a start port: ")
+endPort = input("Enter a end port:")
 
-checkPortsSocket('localhos',[21,22,80,8080,443])
+print("Scanning target: ", target)
+time_init = datetime.now()
+
+try:
+    for port in range(int(startPort),int(endPort)):
+        print("Checking Port {} ...".format(port))
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((target,port))
+        if result == 0:
+            print("Port {} : Open".format(port))
+        else:
+            print("Port {}: close".format(port))
+            print("Reason: ",errno.errorcode[result])
+
+        sock.close()
+
+except socket.error:
+    print("Couldn't connect to server")
+    sys.exit()
+
+time_finish = datetime.now()
+total = time_finish - time_init
+
+print('Port Scanning Completed in: ', total)
